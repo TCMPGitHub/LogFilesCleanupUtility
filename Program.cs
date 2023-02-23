933fd718ee10d613dd0e53fbe4122e6d0625eee0
TCMPGitHub <zhx045@health.ucsd.edu>
2023-02-23 15:44:03 -0800

    Modify email body message

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ namespace LogFilesCleanupUtility
             {
                 string files = path[t].ToString();
                 System.IO.DirectoryInfo di = new DirectoryInfo(files);
+                var count = 0;
 
                 foreach (FileInfo file in di.GetFiles())
                 {
@@ -31,80 +32,80 @@ namespace LogFilesCleanupUtility
                     if (file.LastWriteTime < DateTime.Now.AddDays(Double.Parse(FileAge))) //can be in prod
                         try
                         {
-                            LogWriter.LogMessageToFile(file.Name + " has been deleted");
-                            mailbody = mailbody + file.Name + Environment.NewLine;
                             file.Delete();
+                            count++;
 
                         }
-                        catch(Exception err)
+                        catch (Exception err)
                         {
                             throw err;
                         }
                         finally { }
                 }
-                
-
+                var msg = count.ToString() + " in " + files + " have been deleted";
+                LogWriter.LogMessageToFile(msg);
+                mailbody = mailbody + msg + Environment.NewLine;
             }
             LogWriter.LogMessageToFile("Send email to TCMP ..." + DateTime.Now.ToString());
             mailbody = mailbody + DateTime.Now.ToString() + " Send email to TCMP ...";
             SendMail();
+            return;
+        }
 
+        private static void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            // Get the unique identifier for this asynchronous operation.
+            String token = (string)e.UserState;
 
-           void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
+            if (e.Cancelled)
             {
-                // Get the unique identifier for this asynchronous operation.
-                String token = (string)e.UserState;
-
-                if (e.Cancelled)
-                {
-                    Console.WriteLine("[{0}] Send canceled.", token);
-                }
-                if (e.Error != null)
-                {
-                    Console.WriteLine("[{0}] {1}", token, e.Error.ToString());
-                }
-                else
-                {
-                    LogWriter.LogMessageToFile(" Message sent.");
-                }
-
-                //mailSent = true;
+                Console.WriteLine("[{0}] Send canceled.", token);
+            }
+            if (e.Error != null)
+            {
+                Console.WriteLine("[{0}] {1}", token, e.Error.ToString());
+            }
+            else
+            {
+                LogWriter.LogMessageToFile(" Message sent.");
             }
 
-            void SendMail()
-            {
-                string ToPeople= ConfigurationManager.AppSettings["ToPeople"];
-                string CCPeople = ConfigurationManager.AppSettings["CCPeople"];
-                
-                SmtpClient client = new SmtpClient("smtp.cdcr.ca.gov");
-                MailAddress from = new MailAddress("FDCDBA3_TCMP@cdcr.ca.gov", "TCMP", System.Text.Encoding.UTF8);
-                // Set destinations for the e-mail message.
+            //mailSent = true;
+        }
 
-                // Specify the message content.
-                MailMessage message = new MailMessage();
-                message.From = from;
-                message.To.Add(ToPeople);
-                message.CC.Add(CCPeople);
-                message.Body = "logs cleaned up " + DateTime.Now + ":";
+        private static void SendMail()
+        {
+            string ToPeople = ConfigurationManager.AppSettings["ToPeople"];
+            string CCPeople = ConfigurationManager.AppSettings["CCPeople"];
 
-                // Include some non-ASCII characters in body and subject.
-                message.Body = mailbody + Environment.NewLine;
-                message.BodyEncoding = System.Text.Encoding.UTF8;
-                message.Subject = "Log Files Cleanup!";
-                message.SubjectEncoding = System.Text.Encoding.UTF8;
-                // Set the method that is called back when the send operation ends.
-                client.SendCompleted += new
-                SendCompletedEventHandler(SendCompletedCallback);
-                // The userState can be any object that allows your callback 
-                // method to identify this send operation.
-                string userState = "Log Cleanup";
-                client.SendAsync(message, userState);
-                string answer = Console.ReadLine();
-                // Clean up.
-                message.Dispose();
-                System.Environment.Exit(0);
-                //return;
-            }
+            SmtpClient client = new SmtpClient("smtp.cdcr.ca.gov");
+            MailAddress from = new MailAddress("FDCDBA3_TCMP@cdcr.ca.gov", "TCMP", System.Text.Encoding.UTF8);
+            // Set destinations for the e-mail message.
+
+            // Specify the message content.
+            MailMessage message = new MailMessage();
+            message.From = from;
+            message.To.Add(ToPeople);
+            message.CC.Add(CCPeople);
+            message.Body = "logs cleaned up " + DateTime.Now + ":";
+
+            // Include some non-ASCII characters in body and subject.
+            message.Body = mailbody + Environment.NewLine;
+            message.BodyEncoding = System.Text.Encoding.UTF8;
+            message.Subject = "Log Files Cleanup!";
+            message.SubjectEncoding = System.Text.Encoding.UTF8;
+            // Set the method that is called back when the send operation ends.
+            client.SendCompleted += new
+            SendCompletedEventHandler(SendCompletedCallback);
+            // The userState can be any object that allows your callback 
+            // method to identify this send operation.
+            string userState = "Log Cleanup";
+            client.SendAsync(message, userState);
+            string answer = Console.ReadLine();
+            // Clean up.
+            message.Dispose();
+            System.Environment.Exit(0);
+            //return;
         }
     }
 }
